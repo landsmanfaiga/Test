9146a91b8353f75318f01de08ca9b8ac5bc47c84
Faiga <faigadevorah@gmail.com>
2024-02-28 20:23:15 -0600

    im just testing this

--- practice_class2_28/Controllers/HomeController.cs
+++ practice_class2_28/Controllers/HomeController.cs
@@ -6,12 +6,7 @@ namespace practice_class2_28.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
