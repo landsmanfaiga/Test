85db555566502264b04b8a3982b33803aa08ddb4
Faiga <faigadevorah@gmail.com>
2024-02-28 20:16:13 -0600

    Add project files.

---
+++ practice_class2_28/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practice_class2_28.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
